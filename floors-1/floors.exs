90a113fe378b31455c7f006de239c3cfc35b9ca2
Leszek Smentek <leszek@prograils.com>
2015-12-18 12:06:03 +0100

    2nd part of task 1

--- floors-1/floors.exs
+++ floors-1/floors.exs
@@ -4,18 +4,44 @@ defmodule Floors do
 		body
 	end
 
+	def val("(") do
+		1
+	end
+
+	def val(")") do
+		-1
+	end
+
+	def val(_) do
+		0
+	end
+
 	def count([]) do
 		0
 	end
 
-	def count([")" | tail]) do
-		count(tail) - 1
+	def count([head | tail]) do
+		count(tail) + val(head)
 	end
 
-	def count(["(" | tail]) do
-		count(tail) + 1
+	def find_first_negative(list) do
+		find_first_negative(list, 0, 0)
+	end
+
+	def find_first_negative(_, current, index) when current == -1 do
+		index
+	end
+
+	def find_first_negative([], _, _) do
+		0
+	end
+
+	def find_first_negative([head | tail], current, index) do
+		find_first_negative(tail, current + val(head), index + 1)
 	end
 end
 
 data = Floors.load("data.txt")
-IO.puts "Result: #{Floors.count(String.codepoints(data))}"
+list = String.codepoints(data)
+IO.puts "Result: #{Floors.count(list)}"
+IO.puts "Result2: #{Floors.find_first_negative(list)}"
